2862fd95a77a277dd7f494327197c2377fc3126b
omeriliski <omeriliski06.@gmail.com>
2020-07-09 00:08:26 +0200

    assigment 11

---
+++ ass11.py
@@ -0,0 +1,18 @@
+# assigment 11
+sum = 0
+a = True
+while a == True:
+    number = input("Please enter a number: ")
+    if  not number.isnumeric():
+        a = True
+    elif int(number) < 0:
+        a = True 
+    else:
+        a = False
+    print(a * "it is an invalid entry. Don't use non-numeric, float, or negative values!")    
+for i in number:
+    sum += (int(i)**len(number))
+if sum == int(number):
+    print(f"{number} is an Armstrong Number")
+else:
+    print(f"{number} is not an Armstrong Number")
